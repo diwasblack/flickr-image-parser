2921f6fa1704d667f9c8999dc1be70b5ed7decb2
Diwas Sharma <diwasblack@gmail.com>
2018-02-02 23:18:24 -0600

    Initial model using asyncio/aiohttp

--- src/tasks.py
+++ src/tasks.py
@@ -1,86 +1,94 @@
 import json
-import requests
 import re
+import logging
+
+from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 from string import ascii_lowercase
-from celery import Celery
 from helpers import get_url, insert_into_db
 
-
-app = Celery('tasks', broker='pyamqp://guest@localhost//')
-app.conf.update(
-    task_routes={
-            'tasks.parse_gps': {'queue': 'gps_queue'},
-        }
-)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
-@app.task
-def parse_gps(photo_json):
+async def parse_gps(photo_json):
     '''
     Parses a photo detail page to get GPS information
     '''
 
+    logger.debug("Parsing GPS info for city: {} photo id: {}".format(
+        photo_json['city'], photo_json['id']))
+
     # Construct url for photo detail page
-    url = 'https://www.flickr.com/photos/{}/{}/'.format(photo_json['owner'], photo_json['id'])
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
+    url = 'https://www.flickr.com/photos/{}/{}/'.format(
+        photo_json['owner'], photo_json['id'])
 
-    # GPS information is stored in javascript. Get that part
-    results = soup.find('script', attrs={'class': 'modelExport'})
-    rstr = results.text
+    async with ClientSession() as session:
+        async with session.get(url) as response:
+            page = await response.text()
 
-    long_regex = re.compile("\"longitude\":(([^,])*),")
-    lat_regex = re.compile("\"latitude\":(([^,])*),")
+            soup = BeautifulSoup(page, 'html.parser')
 
-    longitude = long_regex.search(rstr).group(1)
-    latitude = lat_regex.search(rstr).group(1)
+            # GPS information is stored in javascript. Get that part
+            results = soup.find('script', attrs={'class': 'modelExport'})
+            rstr = results.text
 
-    # Create a row to be inserted into db
-    row = (photo_json['filename'], longitude, latitude)
+            long_regex = re.compile("\"longitude\":(([^,])*),")
+            lat_regex = re.compile("\"latitude\":(([^,])*),")
 
-    return insert_into_db(row)
+            longitude = long_regex.search(rstr).group(1)
+            latitude = lat_regex.search(rstr).group(1)
+
+            # Create a row to be inserted into db
+            row = (photo_json['filename'], longitude, latitude)
+
+            return insert_into_db(row)
 
 
-@app.task
-def parse(per_page, page, city):
+async def parse(per_page, page, city):
     '''
     Gets all photos in a particular page for a given city
     And asychronously parses each of these photos to get GPS information
     '''
 
+    logger.debug("Parsing page {} for city: {}".format(page, city))
+
     page_url = get_url(per_page, page, city)
-    response = requests.get(page_url)
-    data = json.loads(response.text)
-    photos = data.get('photos').get('photo')
+    async with ClientSession() as session:
+        async with session.get(page_url) as response:
+            response = await response.text()
+            data = json.loads(response)
+            photos = data.get('photos').get('photo')
 
-    # Enqueue all of the photos into a gps_queue queue
-    for photo in photos:
-        photo_json = {"city": city, "owner": photo.get('owner'), 'id': photo.get('id')}
-        # Get filename/URL
-        for c in ascii_lowercase:
-            key = 'url_' + c
-            if key in photo:
-                photo_json['filename'] = photo.get(key)
-                break
-        parse_gps.delay(photo_json)
+            for photo in photos:
+                photo_json = {"city": city, "owner": photo.get(
+                    'owner'), 'id': photo.get('id')}
+                # Get filename/URL
+                for c in ascii_lowercase:
+                    key = 'url_' + c
+                    if key in photo:
+                        photo_json['filename'] = photo.get(key)
+                        break
+                await parse_gps(photo_json)
 
 
-@app.task
-def parse_city_info(city):
+async def parse_city_info(city):
     '''
     Gets the meta info i.e. total number of pages for a city
     And asynchronously parses each of these pages
     '''
 
-    per_page = 100
+    logger.debug("Getting meta data from city: {}".format(city))
+
+    per_page = 1
     page = 1
 
     url = get_url(per_page, page, city)
-    response = requests.get(url)
-    data = json.loads(response.text)
-    # Get total # of pages
-    pages = data.get('photos').get('pages')
-    # Enqueue all of the pages into a celery queue
-    for i in range(1, int(pages) + 1):
-        parse.delay(per_page, i, city)
+    async with ClientSession() as session:
+        async with session.get(url) as response:
+            response = await response.text()
+            data = json.loads(response)
+            pages = data.get('photos').get('pages')
+            for i in range(1, int(pages) + 1):
+                await parse(per_page, i, city)
